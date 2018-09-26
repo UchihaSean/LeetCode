9aafe5186cb55e680cf3460e274ecb2baac25678
季灵阳 <sean@jilingyangdeMacBook-Pro.local>
2018-09-25 21:32:50 -0400

    Facebook

--- 0708/InsertIntoCysclicSortedLink.py
+++ 0708/InsertIntoCysclicSortedLink.py
@@ -1,26 +1,26 @@
 class Solution(object):
-	def insert(self, head, insertVal):
-		"""
-		:type head: Node
-		:type insertVal: int
-		:rtype: Node
-		"""
+    def insert(self, head, insertVal):
+        """
+        :type head: Node
+        :type insertVal: int
+        :rtype: Node
+        """
 
-		new_node = Node(insertVal, None)
-		if not head:
-			new_node.next = new_node
-			return new_node
+        new_node = Node(insertVal, None)
+        if not head:
+            new_node.next = new_node
+            return new_node
 
-		cur = head
+        cur = head
 
-		while not (cur.val < insertVal <= cur.next.val):
+        while not (cur.val < insertVal <= cur.next.val):
 
-			# cur is max, and cur.next is min. If max < v or min >= v, we stop
-			if cur.next.val <= cur.val:
-				if cur.val < insertVal or cur.next.val >= insertVal:
-					break
+            # cur is max, and cur.next is min. If max < v or min >= v, we stop
+            if cur.next.val <= cur.val:
+                if cur.val < insertVal or cur.next.val >= insertVal:
+                    break
 
-			cur = cur.next
+            cur = cur.next
 
-		new_node.next, cur.next = cur.next, new_node
-		return head
+        new_node.next, cur.next = cur.next, new_node
+        return head
